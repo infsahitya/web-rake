c20447be823c5898511ab20abc8fd70f4cfdb84f
infsahitya <sahitya.api@gmail.com>
2024-07-12 19:33:52 +0530

    update

--- src/module/csv/csv.service.ts
+++ src/module/csv/csv.service.ts
@@ -1,20 +1,27 @@
-import path from "path";
 import * as fs from "fs";
+import * as path from "path";
 import { Parser } from "json2csv";
 import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export default class CsvService {
+  private dirPath: string;
+
+  constructor() {
+    const tempDirPath = path.join(__dirname, "../../..", "docs");
+
+    this.dirPath = tempDirPath;
+
+    if (!fs.existsSync(tempDirPath)) {
+      fs.mkdirSync(tempDirPath, { recursive: true });
+    }
+  }
+
   convertToCsv(data: JobProps[]): string {
     const json2csvParser = new Parser();
     const csv = json2csvParser.parse(data);
 
-    const dirPath = path.join(__dirname, "../../", "docs");
-    const filePath = path.join(dirPath, `${Date.now()}-jobs.csv`);
-
-    if (!fs.existsSync(dirPath)) {
-      fs.mkdirSync(dirPath, { recursive: true });
-    }
+    const filePath = path.join(this.dirPath, `${Date.now()}-jobs.csv`);
 
     fs.writeFileSync(filePath, csv);
     return filePath;
